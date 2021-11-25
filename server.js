ed0efede125ff2cc9d0f779e1fcc93b7c33c0519
Xovos <74909841+CasperDondersPXL@users.noreply.github.com>
2021-11-25 18:04:26 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const app = require("./app");
+const { config } = require("dotenv");
+
+config({
+    path: __dirname + "/.env"
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port);
-console.log("Server running (3000)");
+console.log(`Server running (${port})`);
